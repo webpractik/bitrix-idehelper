8b9dca475bac82f7e43c711d6b36e0bb438c8cb5
Иван Поддубный <north.leshiy@gmail.com>
2019-01-15 17:12:53 +0300

    Update bitrix-idehelper.php

--- bitrix-idehelper.php
+++ bitrix-idehelper.php
@@ -1,367 +1,70 @@
 <?php
 
-namespace Bitrix\Main\ORM\Fields {
+namespace Bitrix\Main\Entity\Field {
+    class  IReadable extends Bitrix\Main\ORM\Fields\IReadable
+    {
+    }
     
-    class IReadable extends Bitrix\Main\Entity\Field\IReadable
+    class IStorable extends Bitrix\Main\ORM\Fields\IStorable
     {
     }
 }
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class IStorable extends Bitrix\Main\Entity\Field\IStorable
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class BooleanField extends Bitrix\Main\Entity\BooleanField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class DateField extends Bitrix\Main\Entity\DateField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class DatetimeField extends Bitrix\Main\Entity\DatetimeField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class EnumField extends Bitrix\Main\Entity\EnumField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class ExpressionField extends Bitrix\Main\Entity\ExpressionField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class IntegerField extends Bitrix\Main\Entity\IntegerField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class FloatField extends Bitrix\Main\Entity\FloatField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class StringField extends Bitrix\Main\Entity\StringField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class TextField extends Bitrix\Main\Entity\TextField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class CryptoField extends Bitrix\Main\Entity\CryptoField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Relations {
-    
-    class Reference extends Bitrix\Main\Entity\ReferenceField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class ScalarField extends Bitrix\Main\Entity\ScalarField
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class Field extends Bitrix\Main\Entity\Field
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields {
-    
-    class FieldError extends Bitrix\Main\Entity\FieldError
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class IValidator extends Bitrix\Main\Entity\IValidator
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class Validator extends Bitrix\Main\Entity\Validator\Base
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class DateValidator extends Bitrix\Main\Entity\Validator\Date
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class EnumValidator extends Bitrix\Main\Entity\Validator\Enum
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class ForeignValidator extends Bitrix\Main\Entity\Validator\Foreign
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class LengthValidator extends Bitrix\Main\Entity\Validator\Length
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class RangeValidator extends Bitrix\Main\Entity\Validator\Range
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class RegExpValidator extends Bitrix\Main\Entity\Validator\RegExp
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Fields\Validators {
-    
-    class UniqueValidator extends Bitrix\Main\Entity\Validator\Unique
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class INosqlPrimarySelector extends Bitrix\Main\Entity\INosqlPrimarySelector
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class NosqlPrimarySelector extends Bitrix\Main\Entity\NosqlPrimarySelector
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class Query extends Bitrix\Main\Entity\Query
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class Chain extends Bitrix\Main\Entity\QueryChain
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class ChainElement extends Bitrix\Main\Entity\QueryChainElement
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Query\Filter\Expressions {
-    
-    class Expression extends Bitrix\Main\Entity\Query\Filter\Expression\Base
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query\Filter\Expressions {
-    
-    class ColumnExpression extends Bitrix\Main\Entity\Query\Filter\Expression\Column
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query\Filter\Expressions {
-    
-    class NullExpression extends Bitrix\Main\Entity\Query\Filter\Expression\NullEx
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Data {
-    
-    class DataManager extends Bitrix\Main\Entity\DataManager
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Data {
-    
-    class Result extends Bitrix\Main\Entity\Result
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Data {
-    
-    class AddResult extends Bitrix\Main\Entity\AddResult
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Data {
-    
-    class UpdateResult extends Bitrix\Main\Entity\UpdateResult
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Data {
-    
-    class DeleteResult extends Bitrix\Main\Entity\DeleteResult
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Query\Filter {
-    
-    class Condition extends Bitrix\Main\Entity\Query\Filter\Condition
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query\Filter {
-    
-    class ConditionTree extends Bitrix\Main\Entity\Query\Filter\ConditionTree
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query\Filter {
-    
-    class Helper extends Bitrix\Main\Entity\Query\Filter\Helper
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query\Filter {
-    
-    class Operator extends Bitrix\Main\Entity\Query\Filter\Operator
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class Expression extends Bitrix\Main\Entity\Query\Expression
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class Join extends Bitrix\Main\Entity\Query\Join
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class Union extends Bitrix\Main\Entity\Query\Union
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM\Query {
-    
-    class UnionCondition extends Bitrix\Main\Entity\Query\UnionCondition
-    {
-    }
-}
-
-
-namespace Bitrix\Main\ORM {
-    
-    class Entity extends Bitrix\Main\Entity\Base
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM {
-    
-    class EntityError extends Bitrix\Main\Entity\EntityError
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM {
-    
-    class Event extends Bitrix\Main\Entity\Event
-    {
-    }
-}
-
-namespace Bitrix\Main\ORM {
-    
-    class EventResult extends Bitrix\Main\Entity\EventResult
-    {
-    }
-}
-
 
 namespace Bitrix\Main\Entity {
     
-    class UField extends Bitrix\Main\ORM\UField
+    class BooleanField extends Bitrix\Main\ORM\Fields\BooleanField
+    {
+    }
+    
+    class DateField extends Bitrix\Main\ORM\Fields\DateField
+    {
+    }
+    
+    class DatetimeField extends Bitrix\Main\ORM\Fields\DatetimeField
+    {
+    }
+    
+    class EnumField extends Bitrix\Main\ORM\Fields\EnumField
+    {
+    }
+    
+    class ExpressionField extends Bitrix\Main\ORM\Fields\ExpressionField
+    {
+    }
+    
+    class IntegerField extends Bitrix\Main\ORM\Fields\IntegerField
+    {
+    }
+    
+    class FloatField extends Bitrix\Main\ORM\Fields\FloatField
+    {
+    }
+    
+    class StringField extends Bitrix\Main\ORM\Fields\StringField
+    {
+    }
+    
+    class TextField extends Bitrix\Main\ORM\Fields\TextField
+    {
+    }
+    
+    class CryptoField extends Bitrix\Main\ORM\Fields\CryptoField
+    {
+    }
+    
+    class ReferenceField extends Bitrix\Main\ORM\Fields\Relations\Reference
+    {
+    }
+    
+    class ScalarField extends Bitrix\Main\ORM\Fields\ScalarField
+    {
+    }
+    
+    class Field extends Bitrix\Main\ORM\Fields\Field
+    {
+    }
+    
+    class FieldError extends Bitrix\Main\ORM\Fields\FieldError
     {
     }
 }
-
