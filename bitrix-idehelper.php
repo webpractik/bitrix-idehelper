6e3d55ccd6cb90c307b31c0b2050339d4706b091
Иван Поддубный <north.leshiy@gmail.com>
2019-01-15 17:25:06 +0300

    Update bitrix-idehelper.php

--- bitrix-idehelper.php
+++ bitrix-idehelper.php
@@ -1,70 +1,70 @@
 <?php
 
 namespace Bitrix\Main\Entity\Field {
-    class  IReadable extends Bitrix\Main\ORM\Fields\IReadable
+    class  IReadable extends \Bitrix\Main\ORM\Fields\IReadable
     {
     }
     
-    class IStorable extends Bitrix\Main\ORM\Fields\IStorable
+    class IStorable extends \Bitrix\Main\ORM\Fields\IStorable
     {
     }
 }
 
 namespace Bitrix\Main\Entity {
     
-    class BooleanField extends Bitrix\Main\ORM\Fields\BooleanField
+    class BooleanField extends \Bitrix\Main\ORM\Fields\BooleanField
     {
     }
     
-    class DateField extends Bitrix\Main\ORM\Fields\DateField
+    class DateField extends \Bitrix\Main\ORM\Fields\DateField
     {
     }
     
-    class DatetimeField extends Bitrix\Main\ORM\Fields\DatetimeField
+    class DatetimeField extends \Bitrix\Main\ORM\Fields\DatetimeField
     {
     }
     
-    class EnumField extends Bitrix\Main\ORM\Fields\EnumField
+    class EnumField extends \Bitrix\Main\ORM\Fields\EnumField
     {
     }
     
-    class ExpressionField extends Bitrix\Main\ORM\Fields\ExpressionField
+    class ExpressionField extends \Bitrix\Main\ORM\Fields\ExpressionField
     {
     }
     
-    class IntegerField extends Bitrix\Main\ORM\Fields\IntegerField
+    class IntegerField extends \Bitrix\Main\ORM\Fields\IntegerField
     {
     }
     
-    class FloatField extends Bitrix\Main\ORM\Fields\FloatField
+    class FloatField extends \Bitrix\Main\ORM\Fields\FloatField
     {
     }
     
-    class StringField extends Bitrix\Main\ORM\Fields\StringField
+    class StringField extends \Bitrix\Main\ORM\Fields\StringField
     {
     }
     
-    class TextField extends Bitrix\Main\ORM\Fields\TextField
+    class TextField extends \Bitrix\Main\ORM\Fields\TextField
     {
     }
     
-    class CryptoField extends Bitrix\Main\ORM\Fields\CryptoField
+    class CryptoField extends \Bitrix\Main\ORM\Fields\CryptoField
     {
     }
     
-    class ReferenceField extends Bitrix\Main\ORM\Fields\Relations\Reference
+    class ReferenceField extends \Bitrix\Main\ORM\Fields\Relations\Reference
     {
     }
     
-    class ScalarField extends Bitrix\Main\ORM\Fields\ScalarField
+    class ScalarField extends \Bitrix\Main\ORM\Fields\ScalarField
     {
     }
     
-    class Field extends Bitrix\Main\ORM\Fields\Field
+    class Field extends \Bitrix\Main\ORM\Fields\Field
     {
     }
     
-    class FieldError extends Bitrix\Main\ORM\Fields\FieldError
+    class FieldError extends \Bitrix\Main\ORM\Fields\FieldError
     {
     }
 }
